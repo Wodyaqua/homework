036f517ef0b2085e61d6c07b216691c203135c7c
Vladimir <vlwasd@yandex.ru>
2021-12-11 13:54:41 +0300

    Fix Fix 3.py

--- lesson_1/3.py
+++ lesson_1/3.py
@@ -1,7 +1,7 @@
 for num in range(1, 101):
     if num % 10 == 1 and num != 11:
         print(f'{num} процент')
-    elif num <= 4:
+    elif num <= 4 or num % 10 == 2 and num != 12 or num % 10 == 3 and num != 13 or num % 10 == 4 and num != 14:
         print(f'{num} процента')
     else:
         print(f'{num} процентов')
